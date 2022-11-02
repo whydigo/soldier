0d58f2a71d9b71a45087689c8ecb93d194f17cb1
whydigo <ukunsss7@gmail.com>
2022-11-02 12:07:40 +0300

    Супер сырая версия

--- soldier_obj.js
+++ soldier_obj.js
@@ -7,6 +7,38 @@ const soldier = {
     },
     ammo: 3,
 }
-soldier.shoot = function() {}
-soldier.reload = function() {}
-soldier.hurt = function() {}
+
+soldier.shoot = function() {
+    if (soldier.gun.cartridges > 0) {
+    soldier.gun.cartridges--
+    console.log('Бах-бах')
+}
+}
+
+soldier.reload = function() {
+    if (soldier.ammo <= 0) {
+        console.log('Не осталось боеприпасов.')
+    } else {
+        console.log('Перезарядка...')
+    }
+    soldier.ammo--
+    soldier.gun.cartridges = 30
+}
+soldier.hurt = function() {
+    if (soldier.health <= 0) {
+        console.log('Ты проиграл.')
+    } else {
+        soldier.health--
+    }
+}
+console.log(`Имя: ${soldier.name}, Здоровье: ${soldier.health}, Название оружия: ${soldier.gun.weapon_name}, Патроны: ${soldier.gun.cartridges}, Боезапасы: ${soldier.ammo}.`)
+
+soldier.shoot()
+console.log(`Патроны: ${soldier.gun.cartridges}, Боезапасы: ${soldier.ammo}.`)
+soldier.reload()
+console.log(`Патроны: ${soldier.gun.cartridges}, Боезапасы: ${soldier.ammo}.`)
+soldier.hurt()
+console.log(`Здоровье: ${soldier.health}`)
+
+console.log(`Имя: ${soldier.name}, Здоровье: ${soldier.health}, Название оружия: ${soldier.gun.weapon_name}, Патроны: ${soldier.gun.cartridges}, Боезапасы: ${soldier.ammo}.`)
+
