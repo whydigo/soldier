3e856b7165cd136c06d73a5ccefb695878be4b70
whydigo <ukunsss7@gmail.com>
2022-11-02 11:05:43 +0300

    Start code

---
+++ soldier_obj.js
@@ -0,0 +1,12 @@
+const soldier = {
+    name: 'John Weak',
+    health: 10,
+    gun: {
+        weapon_name: 'Glock-17',
+        cartridges: 30,
+    },
+    ammo: 3,
+}
+soldier.shoot = function() {}
+soldier.reload = function() {}
+soldier.hurt = function() {}
